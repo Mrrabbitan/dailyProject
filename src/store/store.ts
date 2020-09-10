3d211e337fa7734f60a9947d0514ab4819f9c216
anzp <anzengping@momenta.ai>
2020-09-10 19:57:16 +0800

    push new dist file from build action

--- src/store/store.ts
+++
@@ -1,5 +0,0 @@
-import createMiddlewares from 'redux-saga'
-import { applyMiddleware, createStore } from 'redux'
-import { tabStateReducer } from './reducer'
-
-export const es6Store = (dispatch) => {}
