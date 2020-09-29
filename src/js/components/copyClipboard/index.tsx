52fcbf1199053b305c7d88885223cc63b5ed8018
anzp <anzengping@momenta.ai>
2020-09-29 19:57:16 +0800

    push new dist file from build action

--- src/js/components/copyClipboard/index.tsx
+++
@@ -1,3 +0,0 @@
-import { CopyClipboard } from './copy'
-
-export default CopyClipboard
--- src/store/action.ts
+++
@@ -1,8 +0,0 @@
-import { useDispatch } from 'react-redux'
-
-export const getTabAction = (state) => {
-  return function () {
-    const dispatch = useDispatch()
-    dispatch({ type: state })
-  }
-}
